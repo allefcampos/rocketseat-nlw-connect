3078ac50ad55445981661a27b8de34377ff1ce8e
allefcampos <allef-cleiton88@hotmail.com>
2025-02-19 00:29:04 -0300

    Estrutura inicial do projeto

---
+++ TechLibrary/TechLibrary.Api/UseCase/Users/Register/RegisterUserUseCase.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation.Results;
+using TechLibrary.Api.Domain;
+using TechLibrary.Api.Infraestructure.DataAccess;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCase.Users.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestUserJson request)
+        {
+            Validate(request);
+
+            var entity = new User
+            {
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password
+            };
+
+            var dbContext = new TechLibraryDbContext();
+            dbContext.Users.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+                AccessToken = "akakka"
+            };
+        }
+
+        private void Validate(RequestUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+
+            var result = validator.Validate(request);
+
+            if (!result.IsValid)
+            {
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+}
