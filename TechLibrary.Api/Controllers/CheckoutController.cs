698e163eff8f744cfe17bbdcc9793f02040f173d
allefcampos <allef-cleiton88@hotmail.com>
2025-02-20 14:41:12 -0300

    Aula 03 - Trabalhando com paginação e filtros
    Nesta aula, vamos para etapa final: trabalhar com o gerenciamento de livros! Vamos aprender como recuperar a lista de livros e trazer o resultado de forma paginada, implementar filtros para uma busca eficiente além de criar todo o fluxo de uma reserva. Ao final dessa aula, seu projeto vai estar finalizado e com todas as funcionalidades prontas.

---
+++ TechLibrary.Api/Controllers/CheckoutController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechLibrary.Api.Services;
+using TechLibrary.Api.UseCase.Checkout;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CheckoutController : ControllerBase
+    {
+        [HttpPost]
+        [Route("{bookId}")]
+        public IActionResult BookCheckout(Guid bookId)
+        {
+            var loggedUser = new LoggedUserService(HttpContext);
+
+            var useCase = new RegisterBookCheckoutUseCase(loggedUser);
+
+            useCase.Execute(bookId);
+
+            return NoContent();
+        }
+    }
+}
