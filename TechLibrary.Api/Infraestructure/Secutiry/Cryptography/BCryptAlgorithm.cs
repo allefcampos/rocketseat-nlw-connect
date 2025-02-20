3314bcc019f8acdcd2221e334325aa2d4656fd5f
allefcampos <allef-cleiton88@hotmail.com>
2025-02-20 00:07:55 -0300

    Aula 02 - Autenticação e segurança com JWT
    Nessa aula, vamos implementar todo o processo de login do usuário. Vamos criptografar a senha para proteger os dados sensíveis e configurar a autenticação gerando e validando tokens de acesso JWT. Além disso, vamos testar todo o fluxo no Swagger para assegurar que a autenticação funcione corretamente. Por fim, vamos criar um filtro de exceções para um tratamento de erros mais estruturado, personalizado e robusto.

---
+++ TechLibrary.Api/Infraestructure/Secutiry/Cryptography/BCryptAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using TechLibrary.Api.Domain;
+
+namespace TechLibrary.Api.Infraestructure.Secutiry.Cryptography
+{
+    public class BCryptAlgorithm
+    {
+        public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool Verify(string password, User user) => BCrypt.Net.BCrypt.Verify(password, user.Password);
+    }
+}
---
+++ TechLibrary.Api/UseCase/Login/DoLoginUseCase.cs
@@ -0,0 +1,38 @@
+﻿using TechLibrary.Api.Infraestructure.DataAccess;
+using TechLibrary.Api.Infraestructure.Secutiry.Cryptography;
+using TechLibrary.Api.Infraestructure.Secutiry.Tokens;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+
+namespace TechLibrary.Api.UseCase.Login
+{
+    public class DoLoginUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestLoginJson request)
+        {
+            var dbContext = new TechLibraryDbContext();
+
+            var entity = dbContext
+                            .Users
+                            .FirstOrDefault(user => user.Email.Equals(request.Email));
+
+            if (entity is null)
+                throw new InvalidLoginException();
+
+            var cryptography = new BCryptAlgorithm();
+            var passwordIsValid = cryptography.Verify(request.Password, entity);
+
+            if (!passwordIsValid)
+                throw new InvalidLoginException();
+
+            var tokenGenerator = new JwtTokenGenerator();
+
+            return new ResponseRegisteredUserJson
+            {
+                Name = entity.Name,
+                AccessToken = tokenGenerator.Generate(entity)
+            };
+        }
+    }
+}
